974c06c14137272ca513edabbeb8a618d4c6dfef
Andreeaa-28-a <aa.solorzano.2025@alumnos.urjc.es>
2026-03-19 17:20:01 +0100

    bateria

--- data_generator.sh
+++ data_generator.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-<<COMMENT
-# En caso de hacerlo como funciones:
-generar_temp() {
-    TEMP=$(( (RANDOM % 41) + 20 ))
-    echo "[$(date +%T)] [TEMP] $TEMP" >> rover_system.log
-}
-COMMENT
-
 while true; do
+	BATERIA=$(( (RANDOM % 30) + 10 ))
+    	echo "[$(date +%T)] [BATERIA] $BATERIA" >> rover_system.log
 
 done

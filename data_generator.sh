248c5bc4139bc519e56a68e714f801a1af707252
irenee37 <i.campo.2025@alumnos.urjc.es>
2026-03-19 17:19:36 +0100

    temperatura

--- data_generator.sh
+++ data_generator.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-<<COMMENT
-# En caso de hacerlo como funciones:
+while true; do
 generar_temp() {
     TEMP=$(( (RANDOM % 41) + 20 ))
     echo "[$(date +%T)] [TEMP] $TEMP" >> rover_system.log
 }
-COMMENT
-
-while true; do
 
 done

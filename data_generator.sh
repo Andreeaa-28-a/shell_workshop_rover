c7761109a2297ec12277ba50671117565086d844
gitlu0306 <la.covarrubias.2025@alumnos.urjc.es>
2026-03-19 17:20:05 +0100

    motores

--- data_generator.sh
+++ data_generator.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-<<COMMENT
 # En caso de hacerlo como funciones:
-generar_temp() {
-    TEMP=$(( (RANDOM % 41) + 20 ))
-    echo "[$(date +%T)] [TEMP] $TEMP" >> rover_system.log
-}
-COMMENT
-
+ 
 while true; do
+    MOTORES=$(( (RANDOM % 4000) + 2000 ))
+    echo "[$(date +%T)] [TEMP] $TEMP" >> rover_system.log
 
 done
